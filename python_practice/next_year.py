7b34f97575db12450cf3f80a233ca3b3d5cdd427
Student-B3 <venices440@gmail.com>
2025-01-21 07:26:16 +0000

    Submit

---
+++ python_practice/next_year.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your Name : ")
+gender = input("Enter your Gender : ")
+age = int(input("Enter your Age : "))
+address = input("Enter your Address: ")
+
+print(f"Great, now I know your {name}, {gender}, {age}, and {address}.I guess I'll see you at {age+1}!")
